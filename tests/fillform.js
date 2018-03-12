c5fc841324fd2cc6f8b78fd6a123d62d7282705b
urasigal@gmail.com <urasigal@gmail.com>
2018-03-12 14:38:30 -0300

    fix

--- tests/fillform.js
+++ tests/fillform.js
@@ -59,12 +59,14 @@ module.exports = {
 		element(by.model('vm.can_process')).click();
 		selectDropDownZen.checkDropDown('Manual', xpathes.broadcasterScalingTypeOuter, xpathes.broadcasterScalingTypeInner);
 		element(by.buttonText('Continue')).click().then(function() {
-			browser.sleep(5000);
-			element(by.model('vm.auth_mode')).click();
-			element(by.xpath('/html/body/div[1]/div/div/form/div[1]/div/div[1]/select/option[4]')).click();
-			element(by.model('vm.ffa_inputs')).click();
-			element(by.model('vm.ffa_outputs')).click();
-			element(by.buttonText('Save')).click();
+			browser.sleep(5000).then(function() {
+				element(by.model('vm.auth_mode')).click();
+				element(by.xpath('/html/body/div[1]/div/div/form/div[1]/div/div[1]/select/option[4]')).click();
+				element(by.model('vm.ffa_inputs')).click();
+				element(by.model('vm.ffa_outputs')).click();
+				element(by.buttonText('Save')).click();
+			});
+			
 		});
 		
   	}
