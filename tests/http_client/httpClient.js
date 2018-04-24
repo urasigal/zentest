8e917cec136c8a17c84cfe2d74dc23bbae8f46fa
urasigal@gmail.com <urasigal@gmail.com>
2018-04-24 10:26:37 -0300

    fix

--- tests/http_client/httpClient.js
+++ tests/http_client/httpClient.js
@@ -6,24 +6,8 @@ const https = require('https');
 
 module.exports = {
 		doGetTo: function(url) {
-			function validURL(url) {
-				  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
-				    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
-				    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
-				    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
-				    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
-				    '(\#[-a-z\d_]*)?$','i'); // fragment locater
-				  if(!pattern.test(str)) {
-				    alert("Please enter a valid URL.");
-				    return false;
-				  } else {
-				    return true;
-				  }
-				}
 			return  (function(){
-				return new Promise(function(resolve, reject) {
-					if(validURL(url))
-					{	  
+				return new Promise(function(resolve, reject) {  
 						https.get(url, (resp) => {
 							  let data = '';
 							 
@@ -42,7 +26,6 @@ module.exports = {
 								console.log("Error: " + err.message);
 								reject("Error while HTTP request");
 							});
-					}
 				}); // End of return expression - return Promise.
 			})();
 		}
