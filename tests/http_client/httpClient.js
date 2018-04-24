82e24985659c11b87fd8ab8f7a867cdb78d07731
urasigal@gmail.com <urasigal@gmail.com>
2018-04-24 10:12:01 -0300

    add HTTP client

---
+++ tests/http_client/httpClient.js
@@ -0,0 +1,50 @@
+// Imports.
+const pathes =  require('../const/xpathes.js');
+const locations = require('../tools_js/selecfunc.js');
+const fillForm = require('../tools_js/fillform.js');
+const https = require('https');
+
+module.exports = {
+		doGetTo: function(url) {
+			function validURL(url) {
+				  var pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+				    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+				    '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+				    '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+				    '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+				    '(\#[-a-z\d_]*)?$','i'); // fragment locater
+				  if(!pattern.test(str)) {
+				    alert("Please enter a valid URL.");
+				    return false;
+				  } else {
+				    return true;
+				  }
+				}
+			return  (function(){
+				return new Promise(function(resolve, reject) {
+					if(validURL(url))
+					{	  
+						https.get(url, (resp) => {
+							  let data = '';
+							 
+							  // A chunk of data has been received.
+							  resp.on('data', (chunk) => {
+							    data += chunk;
+							  });
+							 
+							  // The whole response has been received. Print out the result.
+							  resp.on('end', () => {
+							    console.log(JSON.parse(data).explanation);
+							    resolve(data);
+							  });
+							 
+						}).on("error", (err) => {
+								console.log("Error: " + err.message);
+								reject("Error while HTTP request");
+							});
+					}
+				}
+				
+			})();
+		}
+}
--- tests/testDrivers/addBroadcasterTestDriver.js
+++ tests/testDrivers/addBroadcasterTestDriver.js
@@ -2,6 +2,7 @@
 const pathes =  require('../const/xpathes.js');
 const locations = require('../tools_js/selecfunc.js');
 const fillForm = require('../tools_js/fillform.js');
+const httpClient = require('../http_client/httpClient.js');
 
 // This module exports function that creates a new cluster in ZEN.
 module.exports = {
@@ -13,7 +14,6 @@ module.exports = {
 				fillForm.fillNewBroadcasterFormAndSave();
 				
 				// Check if cluster was added.
-				
 				var broadcasterPannel = locations.broadcasterDashboardSelector();
 					browser.sleep(5000).then(function(){
 						return broadcasterPannel.click();
@@ -21,6 +21,7 @@ module.exports = {
 						return fillForm.parseBroadcasterTable('broadcasterqa');
 						}).then(function(res){
 								console.log('The res is ' + res);
+								httpClient.doGetTo('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
 								resolve(res);
 							},function(res){
 								console.log('The res is ' + res);
