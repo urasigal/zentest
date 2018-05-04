f109c2d27801e27de5d86d10babf6b9919c7a1ab
urasigal@gmail.com <urasigal@gmail.com>
2018-05-03 22:36:35 -0300

    fix

--- tests/http_client/httpClient.js
+++ tests/http_client/httpClient.js
@@ -17,6 +17,7 @@ module.exports = {
 								  });
 								  // The whole response has been received. Print out the result.
 								  resp.on('end', () => {
+									 console.log(data);
 								    console.log(JSON.parse(data));
 								    resolve(data);
 								  }); 
